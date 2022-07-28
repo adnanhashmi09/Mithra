d166fa0ed5c673a5c928b5dbd274609ee80ab210
decfox <decfox@github.com>
2022-07-28 13:01:45 +0530

    config: configured AWS(optional) for hermes server

--- Hermes/server/controllers/controller.go
+++ Hermes/server/controllers/controller.go
@@ -13,26 +13,21 @@ type Response struct {
 }
 
 var (
-	mh *db.MongoHandler
-	// s3b               *db.S3Bucket
+	mh                *db.MongoHandler
+	s3b               *db.S3Bucket
 	mongoDbConnection string
-	pinata_key        string
-	pinata_secret     string
-	// aws_region        string
-	// aws_key           string
-	// aws_secret        string
-	// s3_bucket         string
+	aws_region        string
+	aws_key           string
+	aws_secret        string
+	s3_bucket         string
 )
 
 func ControllersInit() {
-	pinata_key = utils.Dotenv("PINATA_API_KEY")
-	pinata_secret = utils.Dotenv("PINATA_API_SECRET")
-
-	// 	aws_region = utils.Dotenv("AWS_REGION")
-	// 	aws_key = utils.Dotenv("AWS_ACCESS_KEY")
-	// 	aws_secret = utils.Dotenv("AWS_ACCESS_SECRET")
-	// s3_bucket = utils.Dotenv("TOKEN_BUCKET_NAME")
-	// s3b, _ = db.NewS3Session(aws_region, aws_key, aws_secret, s3_bucket)
+	aws_region = utils.Dotenv("AWS_REGION")
+	aws_key = utils.Dotenv("AWS_ACCESS_KEY")
+	aws_secret = utils.Dotenv("AWS_ACCESS_SECRET")
+	s3_bucket = utils.Dotenv("TOKEN_BUCKET_NAME")
+	s3b, _ = db.NewS3Session(aws_region, aws_key, aws_secret, s3_bucket)
 
 	mongoDbConnection = utils.Dotenv("DB_URI")
 	mh, _ = db.NewMongoHandler(mongoDbConnection)
